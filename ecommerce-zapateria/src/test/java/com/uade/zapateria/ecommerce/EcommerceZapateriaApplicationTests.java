bfc1ad8eb72a2cda55372fbe97c9d76eaef08440
NadiaTrotvilStudent <ntrotvil@uade.edu.ar>
2026-04-22 10:48:51 -0300

    Initial commit

---
+++ ecommerce-zapateria/src/test/java/com/uade/zapateria/ecommerce/EcommerceZapateriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uade.zapateria.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceZapateriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
